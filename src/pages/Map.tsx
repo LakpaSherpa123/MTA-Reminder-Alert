34038a971415586a75a9cde60e8135e0b148324a
iamlakpa2053 <iamlakpa2053@gmail.com>
2025-01-21 04:26:55 +0000

    Integrated Google map api for transit data

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import { Navbar } from '../Components/Navbar';
+
+
+import { DirectionsRenderer, GoogleMap, TransitLayer, useJsApiLoader } from '@react-google-maps/api';
 
 export const MapPage = () => {
-    return (
+
+    // console.log(process.env.REACT_APP_GOOGLEMAP_API_KEY)
+
+    const { isLoaded } = useJsApiLoader({
+        id: import.meta.env.GOOGLEMAP_API_KEY,
+        googleMapsApiKey: 'AIzaSyAcTBRGIFKleG8VRP7iWt7as-6yubf0kck'
+      
+    })
+
+
+    const containerStyle = {
+        width: '100%',
+        height: '90VH'
+    }
+
+    const center = {
+        lat: 40.756952937702906,
+        lng: -73.98429783140546
+    }
+
+  
+    
+    return isLoaded && (
         <div>
-           
-            <h1>Map Page</h1>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={13}
+            >
+           <TransitLayer />
+            </GoogleMap>
         </div>
     );
 }
